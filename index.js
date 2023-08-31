086acf927dd71a1fef400fac8a7aa4f8249b41b4
Ricardo Behrens <behrensricardo@gmail.com>
2023-08-30 18:26:10 -0600

    trying to figure out how to create the polygons using svg but I have no fucking clue...

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Pseudo Code
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./library/generate');
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "characters",
+                message: "Type the characters in the logo (3 max)",
+            },
+            {
+                type: "input",
+                name: "text",
+                message: "Please type in the text's color (you can also type in the hexadecimal number)",
+            },
+            {
+                type: "list",
+                name: "shape",
+                message: "Please select which shape you'd like the logo to be:",
+                choices: ["circle", "Triangle", "Square"]
+            },
+            {
+                type: "input",
+                name: "text",
+                message: "Please type in the shape's color (you can also type in the hexadecimal number)",
+            },
+        ])
+
+        .then((data) => {
+            const logoContent = markdown(data);
+
+            fs.writeFile('logo.svg', logoContent, (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Generated logo.svg');
+                }
+            })
+        });
+
+}
+
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered
---
+++ library/shapes.js
@@ -0,0 +1,5 @@
+class Polygon {
+    constructor(numberSides, height, width) {
+        this.numberSides= numberSides;
+    }
+}
